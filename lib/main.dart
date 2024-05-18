5a398c5cef9d8a0b14743675c0df93809e4b9d0e
JieiGarcia <jiei.aca@gmail.com>
2024-05-18 17:59:52 +0900

    added a pic,name etc...

--- lib/main.dart
+++ lib/main.dart
@@ -16,22 +16,79 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage("image/profile.jpg"),
-                    radius: 50,
-                  ),
-                ],
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage("image/profile.jpg"),
+                radius: 50,
               ),
-              Text(data),
+              Text(
+                style: TextStyle(
+                    fontFamily: 'PixelifySans',
+                    fontSize: 40.0,
+                    color: Colors.white),
+                'Jiei garcia',
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSans3',
+                    color: Colors.white,
+                    fontSize: 20,
+                    letterSpacing: 4,
+                    fontWeight: FontWeight.bold),
+              ),
               Container(
-                color: Colors.blue,
-                width: 200,
-                height: 50,
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone_android,
+                      color: Colors.teal,
+                      size: 20,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+81 08098885522',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'SourceSans3',
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Jiei.flutter@gmail.com',
+                      style: TextStyle(
+                          fontFamily: 'SourceSans3',
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
               )
             ],
           ),
